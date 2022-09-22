adc7f8dc1f2b5fa05aa3f4715a60a027d75890cf
simplygeo <93863563+simplygeo@users.noreply.github.com>
2022-09-22 19:37:05 +0700

    Update postgrest-sample.sql

--- postgrest-sample.sql
+++ postgrest-sample.sql
@@ -1,7 +1,8 @@
+```sql
 -- things have to be replaced
 -- 1. authenticator pwd: __authenticator_password__
 -- 2. jwt key: __jwt_key__
--- 3. authenticated user: __authenticated_user__
+-- 3. authenticated user: __sample_user__
 -- 4. dbname: __db_name__
 
 -- notes
@@ -36,28 +37,53 @@ create table api.todos (
 insert into api.todos (task) values
   ('finish tutorial 0'), ('pat self on back');
   
+  
+-- spatial table example
+CREATE TABLE api.gpstrack (
+	id serial NOT NULL primary key,
+	ts timestamp NULL,
+	geom public.geometry(point, 4326) NULL,
+	altitude int4 NULL,
+	hdop int4 NULL,
+	vdop int4 NULL,
+	pdop int4 NULL,
+	nstas int4 NULL,
+	speed int4 NULL,
+	course int4 NULL,
+	vid varchar(50) NULL,
+	tscreated timestamptz NULL DEFAULT timezone('Asia/Jakarta'::text, CURRENT_TIMESTAMP),
+	comsrc varchar(3) NULL
+);
+
+insert into api.gpstrack(ts, geom, vid, speed, course) values(now(), 'SRID=4326;POINT(106.3 -6.8)', 'suneo', 40, 23);
+
 
 -- web anon & authenticator
 -- web anon may access todos table, read-only
-drop role if exists web_anon;
+-- drop role if exists web_anon;
 create role web_anon nologin;
 
 grant usage on schema api to web_anon;
 grant select on api.todos to web_anon;
 
-drop role  if exists authenticator;
+-- drop role  if exists authenticator;
 create role authenticator noinherit login password '__authenticator_password__';
 grant web_anon to authenticator;
 
 
 -- authenticated user
-drop role if exists __authenticated_user__;
-create role __authenticated_user__ nologin;
-grant __authenticated_user__ to authenticator;
+-- drop role if exists __sample_user__;
+create role __sample_user__ nologin;
+grant __sample_user__ to authenticator;
 
-grant usage on schema api to __authenticated_user__;
-grant all on api.todos to __authenticated_user__;
-grant usage, select on sequence api.todos_id_seq to __authenticated_user__;
+grant usage on schema api to __sample_user__;
+
+grant all on api.todos to __sample_user__;
+grant usage, select on sequence api.todos_id_seq to __sample_user__;
+
+grant all on api.gpstrack to __sample_user__;
+grant usage, select on sequence api.gpstrack_id_seq to __sample_user__;
+
 
 
 -- storing user & password for login purposes
@@ -83,7 +109,7 @@ begin
 end
 $$ language plpgsql;
 
-drop trigger if exists ensure_user_role_exists on basic_auth.users;
+-- drop trigger if exists ensure_user_role_exists on basic_auth.users;
 
 create constraint trigger ensure_user_role_exists
   after insert or update on basic_auth.users
@@ -100,7 +126,7 @@ create or replace function
     end
     $$ language plpgsql;
 
-drop trigger if exists encrypt_pass on basic_auth.users;
+-- drop trigger if exists encrypt_pass on basic_auth.users;
 
 create trigger encrypt_pass
       before insert or update on basic_auth.users
@@ -123,8 +149,8 @@ create or replace function
 
 
 insert into basic_auth.users(email, pass, role)
-    values('__authenticated_user__@gmail.com', '__authenticated_user__', 		 		
-    '__authenticated_user__');
+    values('__sample_user__@gmail.com', '__sample_user__', 		 		
+    '__sample_user__');
 
 
 CREATE TYPE basic_auth.jwt_token AS (
@@ -156,10 +182,10 @@ create or replace function
     end;
     $$ language plpgsql security definer;	
     
-    
-    
-    -- additional: auto reload schema cache
-    -- Create an event trigger function
+	
+	
+	-- additional: auto reload schema cache
+	-- Create an event trigger function
 CREATE OR REPLACE FUNCTION public.pgrst_watch() RETURNS event_trigger
   LANGUAGE plpgsql
   AS $$
@@ -170,10 +196,11 @@ $$;
 
 -- This event trigger will fire after every ddl_command_end event
 
-DROP EVENT TRIGGER IF EXISTS pgrst_watch;
+-- DROP EVENT TRIGGER IF EXISTS pgrst_watch;
 
 CREATE EVENT TRIGGER pgrst_watch
   ON ddl_command_end
   EXECUTE PROCEDURE public.pgrst_watch();
-    
-    
+	
+	
+	
